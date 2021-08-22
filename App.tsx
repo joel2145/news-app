4969199f1251af8ee15ebcd2b351cd6398d8621c
KikuchiYuta <kikkun0513@gmail.com>
2021-08-22 18:20:27 +0900

    APIからニュース情報を取得・表示

--- App.tsx
+++ App.tsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, FlatList, SafeAreaView } from "react-native";
+import axios from "axios";
+
 import { ListItem } from "./components/ListItem";
-import articles from "./dummies/articles.json";
+import Constants from "expo-constants";
+
+const URL = `https://newsapi.org/v2/top-headlines?country=jp&apiKey=${Constants.manifest.extra.newsApiKey}`;
 
 export default function App() {
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    fetchArticles();
+  }, []);
+
+  const fetchArticles = async () => {
+    try {
+      const response = await axios.get(URL);
+      setArticles(response.data.articles);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  type Props = {
+    imageUrl: string;
+    title: string;
+    author: string;
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
